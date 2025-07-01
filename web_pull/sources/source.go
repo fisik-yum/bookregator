8187ed982ba77b76e3eebf3c25e0f6ccda3441b0
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2025-06-30 17:11:43 -0700

    feat: add csv tags for the dumping stage

--- web_pull/sources/source.go
+++ web_pull/sources/source.go
@@ -2,12 +2,12 @@ package sources
 
 // Struct for itemized reviews
 type ItemizedReview struct {
-	Olid       string          `json:"olid"`
-	Source     string          `json:"source"`
-	ExternalID string          `json:"external_id"`
-	Username   string          `json:"username"`
-	Rating     float64 `json:"rating"`
-	Text       string `json:"text"`
+	Olid       string  `json:"olid" csv:"olid"`
+	Source     string  `json:"source" csv:"source"`
+	ExternalID string  `json:"external_id" csv:"external_id"`
+	Username   string  `json:"username" csv:"username"`
+	Rating     float64 `json:"rating" csv:"rating"`
+	Text       string  `json:"text" csv:"text"`
 }
 type Source interface {
 	GetReviews(isbn string) ([]ItemizedReview, error)
