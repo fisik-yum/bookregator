59c2a1642f76cdad00bd0b341025e2e281f86e1c
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2025-09-02 19:26:47 +0530

    switch to bootstrap 3, will migrate to bootstrap 5

--- front/htmlbuilder/book.go
+++ front/htmlbuilder/book.go
@@ -3,22 +3,21 @@ package htmlbuilder
 import (
 	"book.buckminsterfullerene.net/common"
 	g "maragu.dev/gomponents"
-	comp "maragu.dev/gomponents/components"
-	html "maragu.dev/gomponents/html"
+	. "maragu.dev/gomponents/html"
 )
 
-func Review(review common.Review) g.Node {
-	return html.P(
-		html.H3(g.Text(review.Username)),
-		g.Text(review.Text))
+func makeReview(review common.Review) g.Node {
+	return Div(Class("panel panel-default"),
+		Div(Class("panel-heading"),
+			g.Text(review.Username),
+		),
+		Div(Class("panel-body"),
+			g.Text(review.Text),
+	),
+	)
 }
 
 func ReviewPage(reviews []common.Review) g.Node {
-	reviewNodes:=make([]g.Node,len(reviews))
-	for i,v:=range reviews{
-		reviewNodes[i]=Review(v)
-	}
-	return comp.HTML5(comp.HTML5Props{
-		Body: reviewNodes,
-	})
+	reviewNodes := g.Map(reviews, makeReview)
+	return Page("Test", reviewNodes...)
 }
--- front/htmlbuilder/generic.go
+++ front/htmlbuilder/generic.go
@@ -1,24 +1,62 @@
 package htmlbuilder
 
 import (
-	 g"maragu.dev/gomponents"
-	 html"maragu.dev/gomponents/html"
+	_ "embed"
+	g "maragu.dev/gomponents"
+	comp "maragu.dev/gomponents/components"
+	. "maragu.dev/gomponents/html"
 )
 
+//go:embed head.embed
+var HTML_IMPORTS_HEAD string
+
 func Navbar() g.Node {
-	return html.Nav(html.Class("navbar"),
-		html.Ol(
-			NavbarItem("Home", "/"),
-			NavbarItem("About", "/about"),
+	return Nav(Class("navbar navbar-default"),
+		Div(Class("container-fluid"),
+			Div(Class("navbar-header"),
+				A(Class("navbar-brand"),
+					g.Text("bookregator"),
+				),
+			),
+			// NOTE: include navbar stuff *inside* this ul tag
+			Div(Class("collapse navbar-collapse"),
+				Ul(Class("nav navbar-nav"),
+					NavbarLink("Home", "/"),
+					NavbarLink("About", "/about"),
+					SearchBarOLID(),
+				),
+			),
 		),
 	)
 }
 
-func NavbarItem(name, path string) g.Node {
-	return html.Li(html.A(html.Href(path), g.Text(name)))
+func NavbarLink(name, path string) g.Node {
+	return Li(A(Href(path), g.Text(name)))
 }
 
-func Disclaimer() g.Node{
-	return html.Footer(g.Text("(c) all non-user generated content on this website is licensed under the CC BY license"))
+func SearchBarOLID() g.Node {
+	return Form(Class("navbar-form navbar-right"),
+		Div(Class("form-group"),
+			Input(Type("text"), Class("form-control"), Placeholder("Search OLID")),
+		),
+		Button(Type("submit"), Class("btn btn-default"),
+			g.Text("Submit"),
+		),
+	)
 }
 
+func Disclaimer() g.Node {
+	return Footer(g.Text("(c) all non-user generated content on this website is licensed under the CC BY license"))
+}
+
+// TODO: abstract some templating stuff over this
+func Page(title string, nodes ...g.Node) g.Node {
+	head := []g.Node{Navbar()}
+	return comp.HTML5(comp.HTML5Props{
+		Title: title,
+		Head: []g.Node{
+			g.Raw(HTML_IMPORTS_HEAD),
+		},
+		Body: append(head, nodes...),
+	})
+}
