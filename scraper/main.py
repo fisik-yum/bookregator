f84bfc0638e28c357b71e94f009e2b97f3749675
fisik_yum <54129197+fisik-yum@users.noreply.github.com>
2025-11-22 14:40:44 -0800

    use sets to deduplicate

--- scraper/main.py
+++ scraper/main.py
@@ -11,28 +11,39 @@ logger = logging.getLogger(__name__)
 
 
 def main():
+    olids=set()
+    isbns=set()
+
     with open("isbns.txt") as f:
         lc=0
         for l in f:
             print(f"Working on line {lc}")
             book=l.strip(" \n\r")
             try:
-                ScrapeAndPost(book)
+                ScrapeAndPost(book,olids,isbns)
             except:
                 lc+=1
                 continue
             lc+=1
 
 
-def ScrapeAndPost(isbn: str):
+def ScrapeAndPost(isbn: str,olids: set, isbns: set):
     # auth setup
     authkeys = ("scraper", "opensesame")
     # get list of scrapers
     scrapers = [goodreads.GRScraper, librarything.LTScraper]
+    if isbn in isbns:
+        return
 
+    isbns.add(isbn)
     # get metadata, and initialize structs
     isbn, olid = olshim.ISBNtoOLIDW(isbn)
+
+    if olid not in olids:
+        return
+
     logger.info(f"Working on (ISBN,OLID), ({isbn},{olid})")
+    olids.add(olid)
 
     route = data.ISBNRouteData(isbn, olid)
     work = olshim.GenerateWorkData(olid)
